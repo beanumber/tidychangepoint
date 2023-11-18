c02b1f86401a36d73ac69ca62af8f42ac4e5cd65
Ben Baumer <ben.baumer@gmail.com>
2023-11-18 09:40:48 -0500

    global variables; #7

--- R/0-FnAGBayesianMDL.R
+++ R/0-FnAGBayesianMDL.R
@@ -1,3 +1,4 @@
+globalVariables("x")
 #-------------------------------------------------------------------------#
 #                                                                         #
 #             Funciones de algoritmos geneticos Bayesian MDL              #
