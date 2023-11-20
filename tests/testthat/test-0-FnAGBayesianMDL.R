e77c081e01007d182441435072ac7225cdd9a419
beanumber <ben.baumer@gmail.com>
2023-11-20 10:50:19 -0500

    added test for RandomKeys

--- tests/testthat/test-0-FnAGBayesianMDL.R
+++ tests/testthat/test-0-FnAGBayesianMDL.R
@@ -1,4 +1,9 @@
-test_that("multiplication works", {
+test_that("revisor_param works", {
   expect_output(revisor_param(param), "valid")
   expect_error(revisor_param("nothing"))
+})
+
+test_that("RandomKeys works", {
+  expect_lte(length(RandomKeys(5)), 5)
+  expect_lte(length(RandomKeys(10)), 10)
 })
