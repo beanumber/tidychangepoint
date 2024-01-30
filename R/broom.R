2c4136c966ae77a56fd478687b11c8977a225f56
beanumber <ben.baumer@gmail.com>
2024-01-30 17:06:09 -0500

    added broom layer for changepoint; #16

--- R/broom.R
+++ R/broom.R
@@ -1,14 +1,17 @@
 #' Broom compatibility layer
 #' @export
+#' @importFrom broom tidy
 #' @seealso [broom::tidy()]
 tidy <- broom::tidy
 
 #' @rdname tidy
 #' @export
+#' @importFrom broom augment
 #' @seealso [broom::augment()]
 augment <- broom::augment
 
 #' @rdname tidy
 #' @export
+#' @importFrom broom glance
 #' @seealso [broom::glance()]
 glance <- broom::glance
