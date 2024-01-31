3f3055228f84553ecccc946d23d3ca75bb775843
beanumber <ben.baumer@gmail.com>
2024-01-31 07:40:34 -0500

    added other changepoint methods; #19

--- R/segment.R
+++ R/segment.R
@@ -1,4 +1,9 @@
-#' Segment a time series using Genetic BMDL heuristic
+#' Segment a time series using a variety of algorithms
+#' @param x a numeric vector coercible into a [stats::ts] object
+#' @param method a character string indicating the algorithm to use
+#' @param ... arguments passed to methods
+#' @return an object of class `changepoint::cpt`, [stats::lm], or [cpt_gbmdl]
+#' @seealso [changepoint::cpt.meanvar()]
 #' @export
 #' @examples
 #' mod_null <- segment(DataCPSim)
@@ -15,18 +20,46 @@ segment.numeric <- function(x, method = "null", ...) {
   if (!stats::is.ts(stats::as.ts(x))) {
     stop("x is not coercible into a ts object.")
   }
-  segment(as.ts(x))
+  segment(as.ts(x), method = method, ... = ...)
 }
 
 #' @rdname segment
 #' @export
+#' @examples
+#' segment(DataCPSim, method = "cpt-pelt")
+#' segment(DataCPSim, method = "cpt-pelt", penalty = "AIC")
+#' segment(DataCPSim, method = "cpt-binseg", penalty = "AIC")
+#' segment(DataCPSim, method = "cpt-segneigh", penalty = "BIC")
+#' segment(DataCPSim, method = "cpt-manual", cpts = c(826))
+#' segment(DataCPSim, method = "cpt-manual", cpts = c(365, 826))
+#' 
 
 segment.ts <- function(x, method = "null", ...) {
+  message(paste("method:", method))
+  n <- length(x)
+  ds <- data.frame(y = x, t = 1:n)
   if (method == "cpt-pelt") {
-    return(changepoint::cpt.meanvar(data = x, method = "PELT"))
+    return(changepoint::cpt.meanvar(data = x, method = "PELT", ...))
+  }
+  if (method == "cpt-binseg") {
+    return(changepoint::cpt.meanvar(data = x, method = "BinSeg", ...))
+  }
+  if (method == "cpt-segneigh") {
+    return(changepoint::cpt.meanvar(data = x, method = "SegNeigh", ...))
+  }
+  if (method == "cpt-manual") {
+    message("\nSegmenting using manually input changepoints...")
+    args <- list(...)
+    cpts <- args[["cpts"]]
+    terms <- paste("(t > ", cpts, ")") |>
+      paste(collapse = "+")
+    form <- stats::as.formula(paste("x ~ ", terms))
+    mod <- stats::lm(form, data = ds)
+    class(mod) <- c("cpt_lm", class(mod))
+    return(mod)
   }
   
-  mod_null <- lm(y ~ 1, data = data.frame(y = x))
+  mod_null <- lm(y ~ 1, data = ds)
   class(mod_null) <- c("cpt_lm", class(mod_null))
   return(mod_null)
 }
