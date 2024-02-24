9acde13399ba54c0c83e1b2ffec6043c60123bce
beanumber <ben.baumer@gmail.com>
2024-02-24 07:37:48 -0500

    added testing infrastructure

---
+++ tests/testthat/test-performance.R
@@ -0,0 +1,39 @@
+test_that("test_sets works", {
+  skip()
+  test_sets <- rep(1:12, 3) |>
+    purrr::map(test_set) |>
+    tibble::enframe(value = "data") |>
+    dplyr::mutate(
+      cpt_true = purrr::map(data, attr, which = "cpt_true"),
+      ncpts_true = purrr::map_int(cpt_true, length),
+      nhpp_true = purrr::map2(data, cpt_true, fit_nhpp),
+      bmdl_true = purrr::map_dbl(nhpp_true, BMDL),
+      pelt = purrr::map(data, segment, method = "cpt-pelt"),
+    )
+  readr::write_rds(test_sets, file = here::here("tests/testthat/test_sets.rda"))
+})
+
+test_that("performance works", {
+  skip()
+  test_sets <- readr::read_rds(here::here("tests/testthat/test_sets.rda"))
+  test_sets <- test_sets |>
+    dplyr::mutate(
+      bmdl_pelt = purrr::map_dbl(pelt, BMDL),
+      genetic = purrr::map(data, segment, method = "cpt-gbmdl", num_generations = 10),
+      bmdl_gbmdl = purrr::map_dbl(genetic, BMDL)
+    )
+  
+  test_long <- test_sets |>
+    dplyr::select(ncpts_true, contains("bmdl")) |>
+    tidyr::pivot_longer(cols = -ncpts_true, names_to = "algorithm", values_to = "bmdl")
+  
+  ggplot2::ggplot(test_long, ggplot2::aes(x = ncpts_true, y = bmdl, color = algorithm)) +
+    ggplot2::geom_point() +
+    ggplot2::geom_smooth(se = 0) + 
+    ggplot2::scale_x_continuous("True number of changepoints") +
+    ggplot2::scale_y_continuous("BDML") +
+    ggplot2::labs(
+      title = "Comparison of BMDL scores across algorithms",
+      subtitle = paste(nrow(test_sets), "test data sets")
+    )
+})
