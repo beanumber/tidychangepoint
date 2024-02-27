482f5f677597a00fe2646d205d45316d06464d9c
beanumber <ben.baumer@gmail.com>
2024-02-27 17:13:21 -0500

    updated tests

--- tests/testthat/test-performance.R
+++ tests/testthat/test-performance.R
@@ -9,6 +9,8 @@ test_that("test_sets works", {
       nhpp_true = purrr::map2(data, cpt_true, fit_nhpp),
       bmdl_true = purrr::map_dbl(nhpp_true, BMDL),
       pelt = purrr::map(data, segment, method = "pelt"),
+      bmdl_pelt = purrr::map_dbl(pelt, BMDL),
+      is_pelt_true = identical(cpt_true, changepoints(pelt))
     )
   readr::write_rds(test_sets, file = here::here("tests/testthat/test_sets.rda"))
 })
@@ -18,9 +20,11 @@ test_that("algs works", {
   test_sets <- readr::read_rds(here::here("tests/testthat/test_sets.rda"))
   test_sets <- test_sets |>
     dplyr::mutate(
-      bmdl_pelt = purrr::map_dbl(pelt, BMDL),
-      genetic = purrr::map(data, segment, method = "gbmdl", num_generations = 10),
-      bmdl_gbmdl = purrr::map_dbl(genetic, BMDL)
+#      genetic = purrr::map(data, segment, method = "gbmdl", num_generations = 10),
+      bmdl_gbmdl = purrr::map_dbl(genetic, BMDL), 
+      is_gbmdl_true = bmdl_gbmdl == bmdl_true,
+      is_gbmdl_better_than_pelt = bmdl_gbmdl < bmdl_pelt,
+      is_gbmdl_better_than_true = bmdl_gbmdl < bmdl_true
     )
   readr::write_rds(test_sets, file = here::here("tests/testthat/test_sets.rda"))
 })
@@ -28,6 +32,23 @@ test_that("algs works", {
 test_that("performance works", {
   skip()
   test_sets <- readr::read_rds(here::here("tests/testthat/test_sets.rda"))
+  test_sets |>
+    dplyr::summarize(
+      num_trials = dplyr::n(),
+      pelt_true = sum(is_pelt_true) / dplyr::n(),
+      gbmdl_true = sum(is_gbmdl_true) / dplyr::n(),
+      gbmdl_better_pelt = sum(is_gbmdl_better_than_pelt) / dplyr::n(),
+      gbmdl_better_true = sum(is_gbmdl_better_than_true)/ dplyr::n()
+    )
+  bad <- test_sets |>
+    dplyr::filter(is_gbmdl_better_than_true) |>
+    head(1)
+  
+  x <- bad$pelt[[1]]
+  y <- bad$genetic[[1]]
+  diagnose(x)
+  diagnose(y)
+  
   test_long <- test_sets |>
     dplyr::select(ncpts_true, contains("bmdl")) |>
     tidyr::pivot_longer(cols = -ncpts_true, names_to = "algorithm", values_to = "bmdl")
@@ -46,15 +67,12 @@ test_that("performance works", {
 test_that("running time works", {
   skip()
   test_sets <- readr::read_rds(here::here("tests/testthat/test_sets.rda"))
-  test_long <- test_sets |>
-    dplyr::mutate(
-      pelt_time = purrr::map_dbl(pelt, purrr::pluck, "elapsed_time"),
-      genetic_time = purrr::map_dbl(genetic, purrr::pluck, "elapsed_time")
-    ) |>
-    dplyr::select(ncpts_true, contains("time")) |>
-    tidyr::pivot_longer(cols = -ncpts_true, names_to = "algorithm", values_to = "time")
   
-  ggplot2::ggplot(test_long, ggplot2::aes(x = ncpts_true, y = time, color = algorithm)) +
+  test_glance <- c(test_sets$pelt, test_sets$genetic) |>
+    purrr::map(glance) |>
+    dplyr::bind_rows()
+  
+  ggplot2::ggplot(test_glance, ggplot2::aes(x = num_cpts, y = elapsed_time, color = algorithm)) +
     ggplot2::geom_point() +
     ggplot2::geom_smooth(se = 0) + 
     ggplot2::scale_x_continuous("True number of changepoints") +
@@ -63,5 +81,34 @@ test_that("running time works", {
       title = "Comparison of running time across algorithms",
       subtitle = paste(nrow(test_sets), "test data sets")
     )
+  
+  test_glance |>
+    dplyr::select(matches("algo|num_cpts|nhpp")) |>
+    dplyr::mutate(nhpp_logLik = as.double(nhpp_logLik)) |>
+    tidyr::pivot_longer(cols = -c(algorithm, num_cpts), names_to = "type", values_to = "value") |>
+    ggplot2::ggplot(ggplot2::aes(x = num_cpts, y = value, color = type)) +
+      ggplot2::geom_point() +
+      ggplot2::geom_smooth(se = 0) +
+      ggplot2::facet_wrap(ggplot2::vars(algorithm))
+  
 })
 
+
+test_that("random works", {
+  random_glance <- test_sets |>
+    dplyr::mutate(
+      random = purrr::map(data, segment, method = "random", num_generations = 100)
+    ) |>
+    dplyr::pull(random) |>
+    purrr::map(glance) |>
+    dplyr::bind_rows()
+  
+  random_glance |>
+    dplyr::select(matches("algo|num_cpts|nhpp")) |>
+    dplyr::mutate(nhpp_logLik = as.double(nhpp_logLik)) |>
+    tidyr::pivot_longer(cols = -c(algorithm, num_cpts), names_to = "type", values_to = "value") |>
+    ggplot2::ggplot(ggplot2::aes(x = num_cpts, y = value, color = type)) +
+    ggplot2::geom_point() +
+    ggplot2::geom_smooth(se = 0) +
+    ggplot2::facet_wrap(ggplot2::vars(algorithm))
+})
