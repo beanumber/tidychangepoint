6be9249440f50912fd32427eac08ac4a03d128c6
Ben Baumer <ben.baumer@gmail.com>
2024-03-18 14:16:42 -0500

    close #62

---
+++ R/mod_cptshift.R
@@ -0,0 +1,72 @@
+#' Regression-based model fitting
+#' @export
+#' @examples
+#' tau <- c(365, 826)
+#' mod <- fit_meanshift(DataCPSim, tau)
+#' logLik(mod)
+#' deg_free(mod)
+#' 
+#' cpts <- c(1700, 1739, 1988)
+#' ids <- time2tau(cpts, lubridate::year(time(CET)))
+#' mod <- fit_meanshift(CET, tau = ids)
+#' glance(mod)
+#' glance(fit_meanshift(CET, tau = ids, trends = TRUE))
+#' glance(fit_meanshift(CET, tau = ids, ar1 = TRUE))
+#' glance(fit_meanshift(CET, tau = ids, trends = TRUE, ar1 = TRUE))
+
+fit_meanshift <- function(x, tau, trends = FALSE, ar1 = FALSE, ...) {
+  n <- length(x)
+  ds <- data.frame(y = as.ts(x), t = 1:n)
+  if (length(tau) < 1) {
+    form <- "y ~ 1"
+  } else {
+    if (trends) {
+      terms <- paste(paste("t * (t >=", tau, ")"), collapse = " + ")
+    } else {
+      terms <- paste(paste("(t >=", tau, ")"), collapse = " + ")
+    }
+    form <- paste("y ~", terms)
+  }
+  
+  out <- stats::lm(stats::as.formula(form), data = ds)
+  if (ar1) {
+    out <- autoregress_errors(out)
+  }
+  out$tau <- tau
+  out$trends <- trends
+  out$ar1 <- ar1
+  class(out) <- c("cptshift", class(out))
+  return(out)
+}
+
+autoregress_errors <- function(mod, ...) {
+  n <- nobs(mod)
+  resid <- mod$residuals
+  
+  phi_hat <- sum(head(resid, -1) * tail(resid, -1)) / sum(resid^2)
+  y_hat <- mod$fitted.values + c(0, phi_hat * head(resid, -1))
+  sigma_hat_sq <- sum((mod$model$y - y_hat)^2) / n
+  
+  out <- mod
+  out$fitted.values <- y_hat
+  out$residuals <- mod$model$y - y_hat
+  return(out)
+}
+
+#' @rdname fit_meanshift
+#' @export
+logLik.cptshift <- function(object, ...) {
+  out <- NextMethod()
+  m <- length(object$tau)
+  params_estimated <- object$rank
+  attr(out, "df") <- m + params_estimated + 1 + (object$ar1)
+  return(out)
+}
+
+#' @rdname fit_meanshift
+#' @export
+glance.cptshift <- function(x, ...) {
+  out <- NextMethod()
+  out |>
+    dplyr::select(-adj.r.squared, -statistic, -p.value, -df, -df.residual, nobs)
+}
--- tests/testthat/test-stats.R
+++ tests/testthat/test-stats.R
@@ -5,4 +5,19 @@ test_that("GA works", {
   expect_s3_class(y, "logLik")
   expect_type(BIC(y), "double")
   expect_type(MDL(y), "double")
+
+  null <- fit_meanshift(CET, tau = NULL)
+  expect_s3_class(null, "cptshift")
+    
+  cpts <- c(1700, 1739, 1988)
+  ids <- time2tau(cpts, lubridate::year(time(CET)))
+  trend_wn <- fit_meanshift(CET, tau = ids, trends = TRUE)
+  expect_equal(round(as.numeric(logLik(trend_wn)), 2), -290.02)
+  expect_equal(round(BIC(logLik(trend_wn)), 2), 650.74)
+  expect_equal(MDL(logLik(trend_wn)), 653.07)
+  
+  trend_ar1 <- fit_meanshift(CET, tau = ids, trends = TRUE, ar1 = TRUE)
+  expect_equal(round(as.numeric(logLik(trend_ar1)), 2), -288.80)
+  expect_equal(round(BIC(trend_ar1), 2), 654.19)
+  expect_equal(MDL(logLik(trend_ar1)), 656.52)
 })
