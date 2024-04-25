e63016cdf34fcef9548c63e7ad6833555300b4f3
Ben Baumer <ben.baumer@gmail.com>
2024-04-25 09:06:43 -0500

    added lognormal to meanshift()

--- R/mod_meanshift.R
+++ R/mod_meanshift.R
@@ -1,6 +1,9 @@
 #' Fast implementation of meanshift model
 #' 
 #' @inheritParams fit_lmshift
+#' @param distribution A character indicating the distribution of the data. 
+#' Should match R distribution function naming conventions 
+#' (e.g., "norm" for the Normal distribution, etc.)
 #' @export
 #' @family model-fitting
 #' @author Xueheng Shi
@@ -17,35 +20,45 @@
 #' mod <- fit_meanshift_ar1(CET, tau = ids)
 #' glance(mod)
 
-fit_meanshift <- function(x, tau, ...) {
+fit_meanshift <- function(x, tau, distribution = "norm", ...) {
   y <- as.numeric(as.ts(x))
   N <- length(y) # length of the series
   m <- length(tau) # Number of CPTs
   
   y_seg <- y |>
     split_by_tau(tau)
-  mu_seg <- y_seg |>
-    purrr::map_dbl(mean)
   seg_len <- y_seg |>
     purrr::map_int(length)
-  y_hat <- rep(mu_seg, seg_len) |>
-    unname()
-
-  sigma_hatsq <- sum((y - y_hat)^2) / N
   
-  regions <- split_by_tau(as.ts(x), tau = tau) |>
-    names()
+  # Use the MLE estimates for the means
+  if (distribution == c("lnorm")) {
+    mu_seg <- y_seg |>
+      purrr::map(log) |>
+      purrr::map_dbl(mean)
+    
+    y_hat <- rep(exp(mu_seg), seg_len)
+    sigma_hatsq <- sum((log(y) - y_hat)^2) / N
+  } else {
+    mu_seg <- y_seg |>
+      purrr::map_dbl(mean)
+    
+    y_hat <- rep(mu_seg, seg_len)
+    sigma_hatsq <- sum((y - y_hat)^2) / N
+  }
   
   out <- mod_default(
     x = y,
     tau = tau,
-    region_params = tibble::tibble(region = regions, param_mu = unname(mu_seg)),
-    model_params = c(
-      sigma_hatsq = sigma_hatsq
+    region_params = tibble::tibble(
+      region = names(y_seg), 
+      param_mu = unname(mu_seg)
     ),
-    fitted_values = y_hat,
-    model_name = "meanshift"
+    model_params = c(sigma_hatsq = sigma_hatsq),
+    fitted_values = unname(y_hat),
+    model_name = "meanshift",
+    distribution = distribution
   )
+  class(out) <- c(paste(out$model_name, distribution, sep = "_"), class(out))
   return(out)
 }
 
