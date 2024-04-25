780f2d4549daefcbd34b1ba1f3cb9d2893d6498a
Ben Baumer <ben.baumer@gmail.com>
2024-04-25 14:04:31 -0500

    close #90

--- R/data.R
+++ R/data.R
@@ -19,7 +19,7 @@
 #'  - `station_id`: 
 #'  - `lat`, `long`: latitude and longitude for the weather station
 #'  - `date`, `year`, `month`, `day`: date variables
-#'  - `rainfall`: daily rainfall (in UNITS) as measured by the weather station
+#'  - `rainfall`: daily rainfall (in cubic centimeters) as measured by the weather station
 #' @references [OpenStreetMap](https://www.openstreetmap.org/index.html?mlat=6.244747&mlon=-75.574828&zoom=12)
 "mde_rain"
 
