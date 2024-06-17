adebf926ad96e3b17e9d5353465e255213e1aec6
beanumber <ben.baumer@gmail.com>
2024-06-17 16:47:48 -0500

    identify #108

--- tests/testthat/test-changepoint.R
+++ tests/testthat/test-changepoint.R
@@ -15,10 +15,6 @@ test_that("changepoint works", {
   expect_true(is_segmenter(x$segmenter))
   expect_true(is_model(x$model))
   
-  y <- segment(DataCPSim, method = "pelt", penalty = "BIC")
-  expect_equal(as.double(logLik(y$segmenter)), as.double(logLik(y$model)))
-  expect_equal(unname(fitness(y)), BIC(y$model))
-  
   x <- segment(DataCPSim, method = "binseg")
   expect_s3_class(x, "tidycpt")
   expect_s4_class(x$segmenter, "cpt")
@@ -27,4 +23,30 @@ test_that("changepoint works", {
   expect_s3_class(tidy(x), "tbl_df")
   expect_s3_class(glance(x), "tbl_df")
   expect_type(changepoints(x), "integer")
+})
+
+test_that("changepoint regions match", {
+  skip()
+#  library(tidychangepoint)
+  y <- segment(DataCPSim, method = "pelt", penalty = "BIC")
+  y$segmenter@param.est
+  y$model$region_params
+  
+  DataCPSim |>
+    tibble::as_tibble() |>
+    dplyr::mutate(
+      region = rep(1:4, times = diff(c(1, changepoints(y), nobs(y$segmenter) + 1))),
+      id = row_number()
+    ) |>
+    dplyr::group_by(region) |>
+    dplyr::summarize(
+      N = dplyr::n(), first = min(id), last = max(id), mean = mean(value), var = var(value)
+    )
+  
+  # https://github.com/beanumber/tidychangepoint/issues/99
+  expect_equal(
+    as.double(logLik(y$segmenter)), 
+    as.double(logLik(y$model))
+  )
+  expect_equal(unname(fitness(y)), BIC(y$model))
 })
