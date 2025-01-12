a51ea7f83a34bdd33d1518ccd98e2add085eb73d
beanumber <ben.baumer@gmail.com>
2025-01-12 15:05:38 -0500

    updated CET

--- NEWS.md
+++ NEWS.md
@@ -1,9 +1,10 @@
 # tidychangepoint 1.0.0
 
 * `mlb_diffs` is now a `tsibble` with various statistics.
-* Added `tidycpt_coverage()` to show algorithms, models, and penalty functions 
+* Added `ls_*()` functions to list algorithms, models, and penalty functions 
 usable in the package. 
 * Added support for SIC (=BIC) and HQC penalty functions.
+* Updated CET to include 2021-2024.
 
 # tidychangepoint 0.0.1
 
--- data-raw/CET.R
+++ data-raw/CET.R
@@ -2,9 +2,16 @@ library(tidyverse)
 
 ## Central England temperature
 
-CET <- read_csv(here::here("data-raw", "CentralEnglandT1659-2020_yearly.csv")) |>
+CET20 <- read_csv(here::here("data-raw", "CentralEnglandT1659-2020_yearly.csv")) |>
   rename(mean_temp = avg) |>
   mutate(year = parse_date(as.character(year), format = "%Y")) |>
+  xts::as.xts()
+
+# usethis::use_data(CET20, overwrite = TRUE, compress = "xz")
+
+CET <- read_table("https://www.metoffice.gov.uk/hadobs/hadcet/data/meantemp_monthly_totals.txt", skip = 4) |>
+  mutate(year = parse_date(as.character(Year), format = "%Y")) |>
+  select(year, mean_temp = Annual) |>
   xts::as.xts()
 
 usethis::use_data(CET, overwrite = TRUE, compress = "xz")
